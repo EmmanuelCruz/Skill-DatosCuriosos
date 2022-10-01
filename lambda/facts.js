ba2c51623a4dc03a204825953df2f82af4a98b9d
EmmanuelCruz <emmanuel_cruzh@ciencias.unam.mx>
2022-09-30 20:59:52 -0500

    Skill de Datos curiosos

---
+++ lambda/facts.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        dato: "Se sabe que Vincent van Gogh vendió una sola pintura",
+        tipo: "Arte"
+    },
+    {
+        dato: "En su juventud, Picasso se mantenía caliente quemando algunos de sus dibujos",
+        tipo: "Arte"
+    },
+    {
+        dato: "La fachada del Louvre se extiende por más de cuatro kilómetros",
+        tipo: "Arte"
+    },
+    {
+        dato: "El lugar más reseco de la Tierra es Calama, en el Desierto de Atacama en Chile. Nunca se ha visto caer ahí una sola gota de lluvia",
+        tipo: "Naturaleza"
+    },
+    {
+        dato: "El oceano Pacífico abarca casi por completo un hemisferio de la superficie de la Tierra",
+        tipo: "Naturaleza"
+    },
+    {
+        dato: "Debido a que al terminar el día el aire está más lleno de polvo que al principio de éste, el Sol se ve más rojo al ponerse que al salir",
+        tipo: "Naturaleza"
+    },
+];
